143bc34f7572dd483bdbb39c05731a0e5185f64e
Vlad Solovey <connectwithvlad@gmail.com>
2022-11-20 20:31:32 +0300

    feat: add findAllByXpath method to Page class

--- pages/page.js
+++ pages/page.js
@@ -15,6 +15,10 @@ class Page {
         return this.driver.wait(until.elementLocated(By.xpath(xpath)), 5000);
     }
 
+    async findAllByXpath(xpath) {
+        return this.driver.wait(until.elementsLocated(By.xpath(xpath)), 5000);
+    }
+
     async clickByXpath(xpath) {
         const element = await this.findByXpath(xpath);
         await element.click();
