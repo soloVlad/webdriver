f2a000d549460fb2dc2013cc4239d575088c02ec
Vlad Solovey <connectwithvlad@gmail.com>
2022-11-20 19:45:42 +0300

    feat: add page class

---
+++ pages/page.js
@@ -0,0 +1,19 @@
+const { By, until } = require("selenium-webdriver");
+
+class Page {
+    constructor(driver) {
+        this.driver = driver;
+    }
+
+    async openPage(url) {
+        await this.driver.get(url);
+
+        return this;
+    }
+
+    async findByXpath(xpath) {
+        return this.driver.wait(until.elementLocated(By.xpath(xpath)), 5000);
+    }
+}
+
+module.exports = Page;
