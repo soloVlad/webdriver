50d75de75d2a236e1e7c6e0b09f8f7e6a4467f35
Vlad Solovey <connectwithvlad@gmail.com>
2022-11-20 20:14:17 +0300

    feat: add homePage class

---
+++ pages/homePage.js
@@ -0,0 +1,19 @@
+const Page = require("./page");
+const { By, Key } = require("selenium-webdriver");
+
+const searchInputFieldXpath = "//div[contains(@class, 'SearchBar-module--visibleDesktop')][not(ancestor::ul)]//input[@placeholder='Search products']";
+
+class HomePage extends Page {
+    async clickSearchInputField() {
+        await this.clickByXpath(searchInputFieldXpath);
+
+        return this;
+    }
+
+    async searchProduct(productName) {
+        await this.findByXpath(searchInputFieldXpath).sendKeys(productName, Key.ENTER);
+
+        // TODO: change to searchResultsPage
+        return this;
+    }
+}
